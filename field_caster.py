7d2f2d794385d627181e5ce73f11696c8ff1aae0
esterlein <esterlein.git@gmail.com>
2024-04-01 00:27:44 -0400

    add simulation field matrix

--- field_caster.py
+++ field_caster.py
@@ -1,3 +1,17 @@
+import numpy as np
+import math
+
 class FieldCaster:
-    def init_matrix():
+    
+    m_simul_matrix = [[]]
+    
+    m_size = 20
+    m_mu = 10
+    m_sigma = 2
+    
+    def init_simul_matrix():
+        m_simul_matrix = np.random.normal(m_mu, m_sigma, size(m_size, m_size))
+        for col in range(m_size):
+            for row in range(m_size):
+                m_simul_matrix[row][col] = trunc(m_simul_matrix[row][col]) + m_mu
         return
